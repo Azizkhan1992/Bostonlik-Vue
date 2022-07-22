173b06d45908768ab77ee58e6f59501beeaa5700
Azizkhan1992 <azizkhannasrullayev1992@gmail.com>
2022-07-22 11:40:55 +0500

    3 change

--- src/views/settings-live/UserSettings.vue
+++ src/views/settings-live/UserSettings.vue
@@ -27,29 +27,6 @@ export default{
 }
 </script>
 <style>
-<<<<<<< HEAD
 
-=======
-.user-settings-container{
-    margin-left: 10px;
-}
 
-.user-settings-table table tr{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-}
-.user-settings-table table tr th{
-    display: block;
-    width: 150px;
-    text-align: left;
-    margin-left: 33px;
-    color: #A0AEC0;
-    margin-top: 5px;
-}
-.user-settings-table table .table-header{
-    width: 100%;
-    background: #F7FAFC;
-}
->>>>>>> 991029e0139e0ddd172d46088d43502a02ed56fc
 </style>
