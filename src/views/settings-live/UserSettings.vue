6903060ed61226e766c383d96c578895f703d48f
Azizkhan1992 <azizkhannasrullayev1992@gmail.com>
2022-08-03 18:56:42 +0500

    23 change

--- src/views/settings-live/UserSettings.vue
+++ src/views/settings-live/UserSettings.vue
@@ -4,7 +4,7 @@
         <div class="settings-header-left">
             <h2>Настройки</h2>
             <div class="header-route">
-                <router-link tag="span" to="/settings">настройки</router-link>
+                <router-link tag="span" to="/settings">Настройки</router-link>
                 <img src="@/assets/src/Vector (1).png" alt="">
                 <p>Пользователи системы</p>
             </div>
@@ -35,7 +35,27 @@
             <hr/>
         </table>
     </div>
-    <div class="user-add"></div>
+    <div class="user-add">
+        <div class="user-footer-left">
+            <button>
+                <img class="footer-left-left-img" src="@/assets/src/Vector (2).png" alt="">
+                Пред.
+            </button>
+            <button>
+                <img class="footer-left-right-img" src="@/assets/src/Vector (1).png" alt="">
+                След.
+            </button>
+        </div>
+        <div class="user-footer-center">
+            <span>Страница</span>
+            <input type="text">
+            <span>из 123</span>
+            <button>
+                <img src="@/assets/src/Vector (1).png" alt="">
+            </button>
+        </div>
+        <button>Создать профиль</button>
+    </div>
 </div>
 </template>
 <script>
@@ -73,57 +93,43 @@ export default{
 .user-settings-header .settings-header-right .personal-keys {
   display: none;
 }
-.user-settings-container .user-settings-header{
-    display: flex;
+.user-settings-container .user-add{
     width: 100%;
-    margin-left: 25px;
-    margin-top: 10px;
-}
-.user-settings-header .settings-header-left{
-    display: flex;
-    flex-direction: column;
-}
-.settings-header-left h2{
-    font-size: 2rem;
-    font-weight: 800;
-}
-.settings-header-left .header-route{
     display: flex;
     flex-direction: row;
-    margin-top: 10px;
+    margin-top: 25px;
+    padding-bottom: 25px;
 }
-table .settings-table{
+.user-add .user-footer-left{
     display: flex;
-    margin-top: 15px;
-    width: 100%;
+    flex-direction: row;
+    width: 30%;
+    margin-left: 25px;
 }
-table .settings-table td{
-    cursor: pointer;
-}
-table .settings-table .td-one{
-    display: block;
-    width: 25%;
-    text-align: left;
-    margin-left: 33px;
-    margin-top: 5px;
-    font-size: 16px;
-    font-weight: 700;
+.user-add .user-footer-left button{
+    position: relative;
+    display: flex;
+    width: 50%;
+    margin-left: 25px;
+    background: #FFFFFF;
+    justify-content: center;
+    height: 50px;
+    align-items: center;
+    border-radius: 15px;
+    font-size: 1rem;
+    font-weight: 600;
     color: #A0AEC0;
 }
-table .settings-table .td-two{
+.user-add .user-footer-left button:hover{
+    color: #1C9E3C;
+}
+.user-add .user-footer-left button .footer-left-left-img{
     display: block;
-    width: 25%;
-    text-align: left;
-    margin-left: 33px;
-    margin-top: 5px;
-    font-size: 16px;
-    font-weight: 700;
-    color: #A0AEC0;
-    margin-left: 7%;
+    margin-right: 25px;
 }
-
-hr{
-    border: 1px solid #E2E8F0
+.user-add .user-footer-left button .footer-left-right-img{
+    display: block;
+    position: absolute;
+    right: 30px;
 }
-
 </style>
